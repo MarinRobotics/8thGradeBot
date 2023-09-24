41c3918c9034e58627930f4eb991ee3be6182f3f
Jonathan Wojtazek <jon.wojtaszek@gmail.com>
2023-09-24 14:19:21 -0700

    fixed sweeper code

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,18 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-  static bool pressed = false;
-  pressed = !pressed;
-  if (pressed) {
-    pros::lcd::set_text(2, "I was pressed!");
-  } else {
-    pros::lcd::clear_line(2);
-  }
+void on_center_button()
+{
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed)
+	{
+		pros::lcd::set_text(2, "I was pressed!");
+	}
+	else
+	{
+		pros::lcd::clear_line(2);
+	}
 }
 
 /**
@@ -23,11 +27,12 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
-  pros::lcd::initialize();
-  pros::lcd::set_text(1, "Hello PROS User!");
+void initialize()
+{
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
 
-  pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -75,36 +80,50 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 float power_multiplier = 0.75;
-void opcontrol() {
-  pros::Controller master(pros::E_CONTROLLER_MASTER);
-  pros::Motor left_front(1);
-  pros::Motor left_back(2);
-  pros::Motor right_front(3, true);
-  pros::Motor right_back(4, true);
-  pros::Motor arm(5);
-  pros::Motor arm(6);
-  pros::Motor arm(7);
-  while (true)
+void opcontrol()
+{
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor left_front(1);
+	pros::Motor left_back(2);
+	pros::Motor right_front(3, true);
+	pros::Motor right_back(4, true);
+	pros::Motor arm(5);
+	pros::Motor right_sweeper(6);
+	pros::Motor left_sweeper(7);
+	while (true)
+	{
+		int arm_power = 100;
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+		{
+			// TODO set limits to not smack the base
+			right_sweeper = arm_power;
+		}
 
-    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-	 if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+		{
+			left_sweeper = -arm_power;
+		}
 
-      int right_power = 0.5 * master.get_analog(ANALOG_RIGHT_Y);
-  int left_power = 0.5 * master.get_analog(ANALOG_LEFT_Y);
-  int arm_power = 100;
+		int right_power = 0.5 * master.get_analog(ANALOG_RIGHT_Y);
+		int left_power = 0.5 * master.get_analog(ANALOG_LEFT_Y);
 
-  right_front = right_power;
-  right_back = right_power;
-  left_front = left_power;
-  left_back = left_power;
+		right_front = right_power;
+		right_back = right_power;
+		left_front = left_power;
+		left_back = left_power;
 
-  if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-    arm = arm_power;
-  } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-    arm = -arm_power;
-  } else {
-    arm = 0;
-  }
-  pros::delay(20);
-}
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+		{
+			arm = arm_power;
+		}
+		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+		{
+			arm = -arm_power;
+		}
+		else
+		{
+			arm = 0;
+		}
+		pros::delay(20);
+	}
 }
