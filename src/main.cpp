3e064a8acfe08c231df6750244685018e574b3eb
Jonathan Wojtazek <jon.wojtaszek@gmail.com>
2023-11-05 18:27:07 -0800

    auton moment

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "pros/misc.h"
+#define M_PI 3.14159265358979323846 /* pi */
 
 /**
  * A callback function for LLEMU's center button.
@@ -7,14 +8,19 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-  static bool pressed = false;
-  pressed = !pressed;
-  if (pressed) {
-    pros::lcd::set_text(2, "I was pressed!");
-  } else {
-    pros::lcd::clear_line(2);
-  }
+
+void on_center_button()
+{
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed)
+	{
+		pros::lcd::set_text(2, "I was pressed!");
+	}
+	else
+	{
+		pros::lcd::clear_line(2);
+	}
 }
 
 /**
@@ -27,13 +33,14 @@ void on_center_button() {
 void initialize() {
 
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "we did it");
+	pros::lcd::set_text(1, "autono");
 
 	pros::Motor left_sweeper_initializer(6, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor right_sweeper_initializer(7, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 	pros::Motor wallright_initializer(8, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor wallleft_initializer(9, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor left_front_initializer(10, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 	pros::delay(100);
 }
@@ -66,7 +73,107 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+
+float inchesToRotations(float inches)
+{
+	return inches / (2 * M_PI * 2);
+}
+float inchesToDegrees(float inches)
+{
+	return inchesToRotations(inches) * 360.0;
+}
+/*
+	drivetrain.turnFor(left, -45, degrees);
+	drivetrain.driveFor(reverse, -700, mm);
+	drivetrain.turnFor(right, -45, degrees);
+	drivetrain.driveFor(reverse, -175, mm);
+	drivetrain.driveFor(forward, -200, mm);
+	drivetrain.turnFor(right, 45, degrees);
+	drivetrain.driveFor(reverse, 700, mm);
+	drivetrain.turnFor(right, 45, degrees);
+	drivetrain.driveFor(reverse, 500, mm);
+	drivetrain.turnFor(right, 30, degrees, false);
+*/
+const float ROBOT_DISTANCE_PER_DEGREE = (10.25 * M_PI) / 360.0 / 2;
+void turn(pros::Motor_Group &left, pros::Motor_Group &right, float robot_turn_degrees)
+{
+	float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * robot_turn_degrees; /* calc from robot_turn_degrees */
+	left.move_absolute(motor_turn_degrees, 100);
+	right.move_absolute(motor_turn_degrees, 100);
+}
+
+void opcontrol()
+{
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+	pros::Motor left_front(4, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor left_back(3, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor right_front(2, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor right_back(1, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
+
+	pros::Motor_Group funny({
+		left_front,
+		left_back,
+		right_front,
+		right_back,
+	});
+	pros::Motor_Group funny_left({
+		left_front,
+		left_back,
+	});
+	pros::Motor_Group funny_right({
+		right_front,
+		right_back,
+	});
+	while (true)
+	{
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT))
+		{
+			// turn(funny_left, funny_right, 90);
+			float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * 90; /* calc from robot_turn_degrees */
+			funny_left.move_relative(inchesToDegrees(12.56), 100);
+			//funny_right.move_relative(motor_turn_degrees, 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT))
+		{
+			// turn(funny_right, funny_left, -90);
+			float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * -90; /* calc from robot_turn_degrees */
+			funny_left.move_relative(inchesToDegrees(-12.56), 100);
+			// funny_right.move_relative(motor_turn_degrees, 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP))
+		{
+			funny.move_relative(inchesToDegrees(12.56), 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN))
+		{
+			funny.move_relative(inchesToDegrees(-12.56), 100);
+		}
+	}
+	funny.move_absolute(inchesToDegrees(12.56), 100);
+	turn(funny_left, funny_right, -45);
+	/*10.25
+
+	turn(funny_left, funny_right, -45);
+
+
+	funny.move_absolute(inchesToDegrees(27.56), 100);
+	drivetrain.turnFor(right, -45, degrees);
+	funny.move_absolute(inchesToDegrees(-6.89), 100);
+	funny.move_absolute(inchesToDegrees(7.87), 100);
+	drivetrain.turnFor(right, 45, degrees);
+	funny.move_absolute(inchesToDegrees(-27.56), 100);
+	drivetrain.turnFor(right, 45, degrees);
+	funny.move_absolute(inchesToDegrees(-19.7), 100);
+	drivetrain.turnFor(right, 30, degrees, false);
+*/
+}
+
+bool myPrint(int16_t line, const char *fmt)
+{
+	pros::lcd::print(line, fmt);
+	pros::delay(20);
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -81,8 +188,11 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-float power_multiplier = 0.75;
-void opcontrol() {
+
+float power_multiplier = 1.0;
+
+void opcontrol2()
+{
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_front(4);
 	pros::Motor left_back(3);
@@ -96,6 +206,7 @@ void opcontrol() {
 
 	pros::Motor wallright(8);
 	pros::Motor wallleft(9);
+
 	bool toggle = false;
 
 	// myPrint(3, "starting");
@@ -135,12 +246,12 @@ void opcontrol() {
 			toggle = !toggle;
 			if (toggle)
 			{
-				left_sweeper.move_absolute(75, 115);
+				right_sweeper.move_absolute(75, 115);
 				pros::lcd::print(0, "i like pizza");
 			}
 			else
 			{
-				left_sweeper.move_absolute(0, 115);
+				right_sweeper.move_absolute(0, 115);
 				pros::lcd::print(0, "off");
 			}
 		}
@@ -150,12 +261,12 @@ void opcontrol() {
 			toggle = !toggle;
 			if (toggle)
 			{
-				right_sweeper.move_absolute(85, -115);
+				left_sweeper.move_absolute(85, -115);
 				pros::lcd::print(0, "i like pizza");
 			}
 			else
 			{
-				right_sweeper.move_absolute(0, 115);
+				left_sweeper.move_absolute(0, 115);
 				pros::lcd::print(0, "off");
 			}
 		}
