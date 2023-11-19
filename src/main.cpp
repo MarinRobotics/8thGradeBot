f823e4d8efd65d10ec00d895f75cec56806c422b
Jonathan Wojtazek <jon.wojtaszek@gmail.com>
2023-11-19 13:11:06 -0800

    i have commited before

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void on_center_button()
 	pressed = !pressed;
 	if (pressed)
 	{
-		pros::lcd::set_text(2, "I was pressed!");
+		pros::lcd::set_text(5, "I was pressed!");
 	}
 	else
 	{
@@ -102,7 +102,7 @@ void turn(pros::Motor_Group &left, pros::Motor_Group &right, float robot_turn_de
 	float robot_turn_distance = ROBOT_DISTANCE_PER_DEGREE * robot_turn_degrees;
 	float motor_turn_degrees = inchesToDegrees(robot_turn_distance * 1.08);
 	left.move_relative(motor_turn_degrees, 100);
-	std::vector<double> start_pos = left.get_target_positions ( );
+	std::vector<double> start_pos = left.get_target_positions();
 	right.move_relative(-motor_turn_degrees, 100);
 
 	while (true)
@@ -122,17 +122,30 @@ void move(pros::Motor_Group &motors, float distance)
 	// get current draws
 	// use motor
 	motors.move_relative(inchesToDegrees(distance), 100);
+	std::vector<double> start_pos = motors.get_target_positions();
 	// loop until current draws are close to get
+	while (true)
+	{
+		pros::delay(10);
+		std::vector<double> current_draw = motors.get_positions();
+		float delta = fabs(current_draw[0] - start_pos[0]);
+		std::cout << "current delta" << delta << "\n";
+		if (delta < 5)
+		{
+			break;
+		}
+	}
 }
 
 void autonomous()
-{	
+{
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 	pros::Motor left_front(4, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor left_back(3, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor right_front(2, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor right_back(1, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
+	pros::Motor arm(5, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 	pros::Motor_Group funny({
 		left_front,
@@ -149,10 +162,36 @@ void autonomous()
 		right_back,
 	});
 
-	 turn(funny_left, funny_right, -90);
-	 turn(funny_left, funny_right, 90);
-	// move(funny, 12.56);
-	// move(funny, -12.56);
+	pros::Motor_Group arm_fun({
+		arm
+	});
+	
+	
+
+	move(funny, -50);
+	turn(funny_left, funny_right, 30);
+	turn(funny_left, funny_right, 30);
+	turn(funny_left, funny_right, 30);
+	pros::delay(50);
+	//move(funny, 6);
+	//turn(funny_left, funny_right, 180);
+	//move(funny, 15);
+	move(funny, -6.5);
+	move(funny, 14);
+	turn(funny_left, funny_right, 180);
+	pros::delay(20);
+	turn(funny_left, funny_right, -55);
+	move(funny, -30);
+	move(arm_fun, 30);
+	//move(funny, -29);
+	
+
+	//turn(funny_left, funny_right, -90);
+	//turn(funny_left, funny_right, 90);
+	//move(funny, 12.56);
+	//turn(funny_left, funny_right, 180);
+	//move(funny, 12.56);
+	//move(funny, -12.56);
 
 	/*10.25
 
@@ -191,9 +230,9 @@ bool myPrint(int16_t line, const char *fmt)
  * task, not resume it from where it left off.
  */
 
-float power_multiplier = 1.0;
+float power_multiplier = 1.5;
 
-void opcontrol2()
+void opcontrol()
 {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_front(4);
@@ -208,7 +247,7 @@ void opcontrol2()
 
 	pros::Motor wallright(8);
 	pros::Motor wallleft(9);
-
+	arm.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_HOLD);
 	bool toggle = false;
 
 	// myPrint(3, "starting");
