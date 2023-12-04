5e0e27bbcf1b3759fbcd233f73dfb6a23460d5ae
Jonathan Wojtazek <jon.wojtaszek@gmail.com>
2023-12-03 16:24:36 -0800

    pneumatic and no more wall :((

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+	#include "main.h"
 #include "pros/misc.h"
 #define M_PI 3.14159265358979323846 /* pi */
 
@@ -15,7 +15,7 @@ void on_center_button()
 	pressed = !pressed;
 	if (pressed)
 	{
-		pros::lcd::set_text(5, "I was pressed!");
+		pros::lcd::set_text(2, "I was pressed!");
 	}
 	else
 	{
@@ -94,50 +94,15 @@ float inchesToDegrees(float inches)
 	drivetrain.driveFor(reverse, 500, mm);
 	drivetrain.turnFor(right, 30, degrees, false);
 */
-const float ROBOT_DISTANCE_PER_DEGREE = (10.25 * M_PI) / 360;
+const float ROBOT_DISTANCE_PER_DEGREE = (10.25 * M_PI) / 360.0 / 2;
 void turn(pros::Motor_Group &left, pros::Motor_Group &right, float robot_turn_degrees)
 {
-	std::vector<std::int32_t> start_draw = left.get_current_draws();
-	// use motor
-	float robot_turn_distance = ROBOT_DISTANCE_PER_DEGREE * robot_turn_degrees;
-	float motor_turn_degrees = inchesToDegrees(robot_turn_distance * 1.08);
-	left.move_relative(motor_turn_degrees, 100);
-	std::vector<double> start_pos = left.get_target_positions();
-	right.move_relative(-motor_turn_degrees, 100);
-
-	while (true)
-	{
-		pros::delay(10);
-		std::vector<double> current_draw = left.get_positions();
-		float delta = fabs(current_draw[0] - start_pos[0]);
-		std::cout << "current delta" << delta << "\n";
-		if (delta < 5)
-		{
-			break;
-		}
-	}
-}
-void move(pros::Motor_Group &motors, float distance)
-{
-	// get current draws
-	// use motor
-	motors.move_relative(inchesToDegrees(distance), 100);
-	std::vector<double> start_pos = motors.get_target_positions();
-	// loop until current draws are close to get
-	while (true)
-	{
-		pros::delay(10);
-		std::vector<double> current_draw = motors.get_positions();
-		float delta = fabs(current_draw[0] - start_pos[0]);
-		std::cout << "current delta" << delta << "\n";
-		if (delta < 5)
-		{
-			break;
-		}
-	}
+	float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * robot_turn_degrees; /* calc from robot_turn_degrees */
+	left.move_absolute(motor_turn_degrees, 100);
+	right.move_absolute(motor_turn_degrees, 100);
 }
 
-void autonomous()
+void opcontrol2()
 {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
@@ -145,7 +110,6 @@ void autonomous()
 	pros::Motor left_back(3, pros::E_MOTOR_GEAR_GREEN, false, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor right_front(2, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 	pros::Motor right_back(1, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
-	pros::Motor arm(5, pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 	pros::Motor_Group funny({
 		left_front,
@@ -161,38 +125,33 @@ void autonomous()
 		right_front,
 		right_back,
 	});
-
-	pros::Motor_Group arm_fun({
-		arm
-	});
-	
-	
-
-	move(funny, -50);
-	turn(funny_left, funny_right, 30);
-	turn(funny_left, funny_right, 30);
-	turn(funny_left, funny_right, 30);
-	pros::delay(50);
-	//move(funny, 6);
-	//turn(funny_left, funny_right, 180);
-	//move(funny, 15);
-	move(funny, -6.5);
-	move(funny, 14);
-	turn(funny_left, funny_right, 180);
-	pros::delay(20);
-	turn(funny_left, funny_right, -55);
-	move(funny, -30);
-	move(arm_fun, 30);
-	//move(funny, -29);
-	
-
-	//turn(funny_left, funny_right, -90);
-	//turn(funny_left, funny_right, 90);
-	//move(funny, 12.56);
-	//turn(funny_left, funny_right, 180);
-	//move(funny, 12.56);
-	//move(funny, -12.56);
-
+	while (true)
+	{
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT))
+		{
+			// turn(funny_left, funny_right, 90);
+			float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * 90; /* calc from robot_turn_degrees */
+			funny_left.move_relative(inchesToDegrees(12.56), 100);
+			//funny_right.move_relative(motor_turn_degrees, 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT))
+		{
+			// turn(funny_right, funny_left, -90);
+			float motor_turn_degrees = ROBOT_DISTANCE_PER_DEGREE * -90; /* calc from robot_turn_degrees */
+			funny_left.move_relative(inchesToDegrees(-12.56), 100);
+			// funny_right.move_relative(motor_turn_degrees, 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP))
+		{
+			funny.move_relative(inchesToDegrees(12.56), 100);
+		}
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN))
+		{
+			funny.move_relative(inchesToDegrees(-12.56), 100);
+		}
+	}
+	funny.move_absolute(inchesToDegrees(12.56), 100);
+	turn(funny_left, funny_right, -45);
 	/*10.25
 
 	turn(funny_left, funny_right, -45);
@@ -230,7 +189,7 @@ bool myPrint(int16_t line, const char *fmt)
  * task, not resume it from where it left off.
  */
 
-float power_multiplier = 1.5;
+float power_multiplier = 1.0;
 
 void opcontrol()
 {
@@ -244,10 +203,10 @@ void opcontrol()
 	pros::Motor right_sweeper(7);
 	pros::Motor left_wall(8);
 	pros::Motor right_wall(9);   
-
 	pros::Motor wallright(8);
-	pros::Motor wallleft(9);
-	arm.set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_HOLD);
+	pros::Motor wallleft(9);	
+	pros::ADIDigitalOut piston('A');
+
 	bool toggle = false;
 
 	// myPrint(3, "starting");
@@ -314,18 +273,28 @@ void opcontrol()
 
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP))
 		{
-			wallright.move_absolute(75, 110);
-			wallleft.move_absolute(75, 110);
-			pros::lcd::print(5, "wall up");
-			pros::delay(20);
+
+			piston.set_value(true);
+			//pros::delay(1000);
+			//piston.set_value(false);
+
+			
+			//wallright.move_absolute(75, 110);
+			//wallleft.move_absolute(75, 110);
+			//pros::lcd::print(5, "wall up");
+			//pros::delay(20);
+			
 		}
 
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN))
 		{
-			wallright.move_absolute(0, 35);
+
+			// piston.set_value(false);jol,8i
+
+			/*wallright.move_absolute(0, 35);
 			wallleft.move_absolute(0, 35);
 			pros::lcd::print(5, "wall down");
-			pros::delay(20);
+			pros::delay(20);*/
 		}
 
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT))
@@ -336,25 +305,23 @@ void opcontrol()
 			pros::delay(20);
 		}
 
-	int right_power = master.get_analog(ANALOG_RIGHT_Y);
-	int left_power = master.get_analog(ANALOG_LEFT_Y);
+		int right_power = master.get_analog(ANALOG_RIGHT_Y);
+		int left_power = master.get_analog(ANALOG_LEFT_Y);
 
-	right_front = right_power;
-	right_back = right_power;
-	left_front = left_power;
-	left_back = left_power;
+			right_front = right_power;
+		right_back = right_power;
+		left_front = left_power;
+		left_back = left_power;
 
-	int arm_power = 100;
+		//int arm_power = 100;
 
-	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
 		arm = arm_power;
-	} else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+		} else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
 		arm = -arm_power;
-	} else {
+		} else {
 		arm = 0;
-	}
-
-	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+		}
 			
 	}
-}
+}		
